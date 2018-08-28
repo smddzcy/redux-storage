3e60fc777cc811fc48c72e4cc269c42a0e9aeac4
Samed Düzçay <samedduzcay@gmail.com>
2018-08-28 21:32:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-export { default as createLoader } from './createLoader';
-export { default as createMiddleware } from './createMiddleware';
-export { default as reducer } from './reducer';
-export { LOAD, SAVE } from './constants';
+import createLoader from './createLoader';
+import createMiddleware from './createMiddleware';
+import reducer from './reducer';
+import { LOAD, SAVE } from './constants';
+
+export {
+    LOAD, SAVE,
+    createLoader,
+    createMiddleware,
+    reducer,
+};
 
 // The full default export is required to be BC with redux-storage <= v1.3.2
 export default {
-    ...require('./constants'),
-    createLoader: require('./createLoader').default,
-    createMiddleware: require('./createMiddleware').default,
-    reducer: require('./reducer').default
+    LOAD, SAVE,
+    createLoader,
+    createMiddleware,
+    reducer,
 };
